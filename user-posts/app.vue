64955a6ddd78bf3d504cee735ca6f953237e680a
Pradeep <pknitn@gmail.com>
2024-12-11 22:11:18 +0530

    configure tailwind css to project

--- user-posts/app.vue
+++ user-posts/app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+   
   </div>
 </template>
---
+++ user-posts/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Navbar from '~/components/Navbar.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <Navbar/>
+        <h1>default layout</h1>
+    </div>
+    <slot/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
