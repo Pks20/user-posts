bd8b4074b4534370adb25e2f157c7601ae83a682
Pradeep <pknitn@gmail.com>
2024-12-12 00:01:49 +0530

    created navbar

--- user-posts/components/Navbar.vue
+++ user-posts/components/Navbar.vue
@@ -3,10 +3,22 @@
 </script>
 
 <template>
-    <div>
-        Navbar
-    </div>
-</template>
+    <nav class="bg-blue-500 p-4">
+      <div class="container mx-auto flex  items-center">
+        <ul>
+          <li>
+            <NuxtLink
+              to="/"
+              class="text-white hover:text-gray-300 transition-colors duration-200"
+            >
+              Home
+            </NuxtLink>
+          </li>
+        </ul>
+        <h1 class="text-white text-lg font-bold mx-auto">Post Application</h1>
+      </div>
+    </nav>
+  </template>
 
 <style lang="scss" scoped>
 
